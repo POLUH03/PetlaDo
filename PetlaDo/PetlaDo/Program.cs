57f8a652a1e3a62d7d4c353aef1fd770e1d489b6
POLUH03 <80423162+POLUH03@users.noreply.github.com>
2024-01-14 15:25:07 +0100

    Add files via upload

---
+++ PetlaDo/PetlaDo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 1;
+
+        do
+        {
+            int square = number * number;
+            Console.WriteLine($"Kwadrat liczby {number}: {square}");
+            number++;
+        } while (number <= 20);
+    }
+}
+
